04e94574b8bf16a29c9f150876ad1ead13ab13aa
Elisha Adeboye <y.adeboye@gmail.com>
2025-01-03 17:13:59 +0100

    centralized beat text

--- react-drum/src/components/Drum.jsx
+++ react-drum/src/components/Drum.jsx
@@ -15,15 +15,27 @@ const Drum = () => {
         <div className="beat">T</div>
       </div>
       <div className="control__panel">
-        <div>
-          <p>Power</p>
+        <div className="power">
+          <p className="control__label">Power</p>
           <label className="switch">
             <input type="checkbox" />
             <span className="slider"></span>
           </label>
         </div>
         <div className="display">Display</div>
-        <div>Volume</div>
+        <div className="volumeDiv">
+          <p className="control__label">Slider</p>
+          <div className="slidecontainer">
+            <input
+              type="range"
+              min="1"
+              max="100"
+              value="50"
+              class="sliderVol"
+              id="myRange"
+            />
+          </div>
+        </div>
         <div>
           <p className="control__label">Bank</p>
           <label className="switch">
