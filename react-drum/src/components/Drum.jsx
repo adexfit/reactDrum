2189f6c2cd650b6a04f1ea4227f8045e870b217d
Elisha Adeboye <y.adeboye@gmail.com>
2025-01-05 21:28:13 +0100

    added sounds

--- react-drum/src/components/Drum.jsx
+++ react-drum/src/components/Drum.jsx
@@ -1,18 +1,146 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Drum = () => {
+  const [soundName, setSoundname] = useState("Display");
+
+  const soundQ = () => {
+    const audio = new Audio(
+      "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3"
+    );
+    setSoundname((pre) => (pre = "Heater 1"));
+    audio.play();
+  };
+  const soundW = () => {
+    const audio = new Audio(
+      "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3"
+    );
+    setSoundname((pre) => (pre = "Heater 2"));
+    audio.play();
+  };
+  const soundE = () => {
+    const audio = new Audio(
+      "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3"
+    );
+    setSoundname((pre) => (pre = "Heater 3"));
+    audio.play();
+  };
+  const soundA = () => {
+    const audio = new Audio(
+      "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3"
+    );
+    setSoundname((pre) => (pre = "Heater 4"));
+    audio.play();
+  };
+  const soundS = () => {
+    const audio = new Audio(
+      "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3"
+    );
+    setSoundname((pre) => (pre = "Clap"));
+    audio.play();
+  };
+  const soundD = () => {
+    const audio = new Audio(
+      "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3"
+    );
+    setSoundname((pre) => (pre = "Open HH"));
+    audio.play();
+  };
+  const soundZ = () => {
+    const audio = new Audio(
+      "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3"
+    );
+    setSoundname((pre) => (pre = "Kic_n_Hat"));
+    audio.play();
+  };
+  const soundX = () => {
+    const audio = new Audio(
+      "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3"
+    );
+    setSoundname((pre) => (pre = "Kick"));
+    audio.play();
+  };
+  const soundC = () => {
+    const audio = new Audio(
+      "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3"
+    );
+    setSoundname((pre) => (pre = "Closed HH"));
+    audio.play();
+  };
   return (
-    <main className="main__card">
+    <main className="main__card" id="drum-machine">
       <div className="beatcard">
-        <div className="beat">Q</div>
-        <div className="beat">R</div>
-        <div className="beat">Y</div>
-        <div className="beat">U</div>
-        <div className="beat">O</div>
-        <div className="beat">P</div>
-        <div className="beat">W</div>
-        <div className="beat">E</div>
-        <div className="beat">T</div>
+        <div className="drum-pad" onClick={soundQ}>
+          Q
+          <audio id="Q" className="clip">
+            <source src="horse.ogg" type="audio/ogg" />
+            <source src="horse.mp3" type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+        <div className="drum-pad" onClick={soundW}>
+          W
+          <audio id="W" className="clip">
+            <source src="horse.ogg" type="audio/ogg" />
+            <source src="horse.mp3" type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+        <div className="drum-pad" onClick={soundE}>
+          E
+          <audio id="E" className="clip">
+            <source src="horse.ogg" type="audio/ogg" />
+            <source src="horse.mp3" type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+        <div className="drum-pad" onClick={soundA}>
+          A
+          <audio id="A" className="clip">
+            <source src="horse.ogg" type="audio/ogg" />
+            <source src="horse.mp3" type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+        <div className="drum-pad" onClick={soundS}>
+          S
+          <audio id="S" className="clip">
+            <source src="horse.ogg" type="audio/ogg" />
+            <source src="horse.mp3" type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+        <div className="drum-pad" onClick={soundD}>
+          D
+          <audio id="D" className="clip">
+            <source src="horse.ogg" type="audio/ogg" />
+            <source src="horse.mp3" type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+        <div className="drum-pad" onClick={soundZ}>
+          Z
+          <audio id="Z" className="clip">
+            <source src="horse.ogg" type="audio/ogg" />
+            <source src="horse.mp3" type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+        <div className="drum-pad" onClick={soundX}>
+          X
+          <audio id="X" className="clip">
+            <source src="horse.ogg" type="audio/ogg" />
+            <source src="horse.mp3" type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+        <div className="drum-pad" onClick={soundC}>
+          C
+          <audio id="C" className="clip">
+            <source src="horse.ogg" type="audio/ogg" />
+            <source src="horse.mp3" type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
       </div>
       <div className="control__panel">
         <div className="power">
@@ -22,8 +150,9 @@ const Drum = () => {
             <span className="slider"></span>
           </label>
         </div>
-        <div className="display">Display</div>
-        {/* <div className="volumeDiv"> */}
+        <div className="display" id="display">
+          {soundName}
+        </div>
 
         <div className="slidecontainer">
           <p className="control__label">Slider</p>
